96deab9886a015d751adda56bd44cf4b717ddd90
Nicholas Botorog <nicholasbotorog@gmail.com>
2024-01-18 13:04:17 -0500

    first commit

---
+++ src/main/java/com/snippets/Snipper/Snippets/API/SnipperSnippetsApiApplication.java
@@ -0,0 +1,13 @@
+package com.snippets.Snipper.Snippets.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnipperSnippetsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnipperSnippetsApiApplication.class, args);
+	}
+
+}
